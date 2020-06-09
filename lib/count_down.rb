b755c70eb6c8932d8f63a0b9ed98fef61149f771
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-09 21:23:51 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -21,8 +21,11 @@
   end
   
   count = 0 
+  magic_exit_number = 7
+  
   
   while count < 10 do 
+    break if count == magic_exit_number
     puts "I am the #{count}, I love to count!"
      count += 1 
     end
